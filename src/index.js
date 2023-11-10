43bdb9459a9329558d96990b43493d1239c67074
AlanCai <p_alanccai@tencent.com>
2023-11-10 13:58:12 +0800

    @路径解析配置，联想路径配置

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import sum from '@/test'
+const total = sum(1, 3)
 
+console.log(total)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
