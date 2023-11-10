0364f3da690c4b7aa61aafdfc411b438fe7dd4d4
AlanCai <p_alanccai@tencent.com>
2023-11-10 15:37:05 +0800

    antD主题定制

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './theme.css'
 import { RouterProvider} from 'react-router-dom'
 import router from './router'
 import sum from '@/test'
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,16 @@
 import {Outlet} from "react-router-dom";
+import {Button} from "antd-mobile";
 
 const Layout = () => {
   return (
     <div>
       <Outlet/>
       我是Layout
+      <Button color='primary'>全局按钮</Button>
+      <div className='puple'>
+        <Button color='primary'>局部按钮</Button>
+      </div>
+
     </div>
   )
 }
